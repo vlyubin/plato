1ec58824d81cfa2cedf4845f798c37d5fb0c0f4a
Volodymyr Lyubinets <vlyubin@gmail.com>
2023-07-30 19:00:24 -0600

    gpt4

--- server/openai_api.py
+++ server/openai_api.py
@@ -2,7 +2,7 @@ import openai
 
 
 def query_openai(messages):
-	chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=messages) # "gpt-3.5-turbo"
+	chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 	return chat_completion.choices[0].message.content
 
 
@@ -18,7 +18,7 @@ def write_speech(person_info: str, topic: str, supporting: bool = True):
     speech_type_keyword = "supporting" if supporting else "rebuttal"
     rebuttal_prompt = " Reminder - you are giving a rebuttal speech, so you want to refute the topic." if not supporting else ""
     system_prompt = ""
-    user_prompt = f"""You are {person_info}. You are particinpating in a debate competition and are writing a {speech_type_keyword} speech for the following statment: "{topic}". Come up with short speech (120 words or less).{rebuttal_prompt}
+    user_prompt = f"""You are {person_info}. You are particinpating in a debate competition and are writing a {speech_type_keyword} speech for the following statment: "{topic}". Where possible, reference your own background and qualifications. Come up with short speech (120 words or less).{rebuttal_prompt}
 Speech:"""
     return query_with_prompts(system_prompt, user_prompt)
 
@@ -28,7 +28,7 @@ def judge_speeches(topic: str, supporting_speech: str, rebuttal_speech: str):
     Uses openai to judge two speeches
     """
     system_prompt = ""
-    user_prompt = f"""You are a judge in a debate competition. Given a topic, supporting and rebuttal speeches, grade each speech on a range from 1 to 10.
+    user_prompt = f"""You are a judge in a debate competition. Given a topic, supporting and rebuttal speeches, grade each speech on a range from 1 to 10. Penalize speeches that too short or make weak arguments.
 Topic: {topic}
 
 Supporting speech: {supporting_speech}
@@ -41,7 +41,7 @@ Supporting speech score, rebuttal speech score (comma-separated):"""
     scores = query_with_prompts(system_prompt, user_prompt)
     print("Judges scores: " + scores)
 
-    user_prompt = f"""You are a judge in a debate competition. Given a topic, supporting and rebuttal speeches, give a brief critique of both speeches.
+    user_prompt = f"""You are a judge in a debate competition. Given a topic, supporting and rebuttal speeches, give a brief critique of both speeches. Penalize speeches that too short or make weak arguments.
 Topic: {topic}
 
 Supporting speech: {supporting_speech}
