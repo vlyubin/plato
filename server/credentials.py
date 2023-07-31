049b089a12c18aa7f6adbf55407bf328da96fba1
Volodymyr Lyubinets <vlyubin@gmail.com>
2023-07-30 18:05:48 -0600

    Access code

--- server/credentials.py
+++ server/credentials.py
@@ -9,6 +9,7 @@ load_dotenv("/var/www/py/plato/server")
 # Fetch API keys from .env file
 OPENAI_KEY = os.getenv('OPENAI_KEY')
 ELEVENLABS_KEY = os.getenv('ELEVENLABS_KEY')
+ACCESS_CODE = os.getenv('ACCESS_CODE')
 
 # Set API keys
 set_api_key(ELEVENLABS_KEY)
